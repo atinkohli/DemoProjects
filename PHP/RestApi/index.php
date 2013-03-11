33c290aee1eb660adee79e0483e02d1b72b0e40a
AtinKohli <atinkohli@yahoo.com>
2013-03-11 01:45:58 +0000

    updates

--- PHP/RestApi/index.php
+++ PHP/RestApi/index.php
@@ -3,10 +3,27 @@ require_once("C:/My Data(Atin)/Personnel/GitHub/DemoProjects/PHP/RestApi/Common/
 require_once(REST_DIR."/rest/RestApiImpl.php");
 require_once(REST_DIR."/Logger/ScreenLogger.php");
 
-    $restObj=new RestApiImpl();
+$restObj=new RestApiImpl();
           
+$reqType=$_SERVER['REQUEST_METHOD'];
+//ScreenLogger::logInfo($reqType);
+
+
+if($reqType=="GET") {
     $restObj->get();
+    ScreenLogger::logInfo($_GET['app']);
+} else if($reqType=="POST") {
+    $restObj->post();
+    ScreenLogger::logInfo($_GET['app']);
+} else if($reqType=="PUT") {
+    $restObj->put();
+} else if ($reqType=="DELETE") {
+    $restObj->delete();
+} else {
+    ScreenLogger::logInfo("Un-Supported Request Type");
+}
+
     
-    ScreenLogger::logInfo("Exit");
+ScreenLogger::logInfo("Exit");
        
 ?>
